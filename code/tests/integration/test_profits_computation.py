98926b74dfb1384fe47d222944cfe1f37ea34d7a
MalcomChumchouaPenda <malcom.chumchouapenda@yahoo.fr>
2026-09-09 01:10:35 +0100

    refactor profits computation integration

--- code/tests/integration/test_profits_computation.py
+++ code/tests/integration/test_profits_computation.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import Mock, PropertyMock
-from model.spaces.country import Country
+from model.base import EcoAccount
 from model.agents.bank import Bank
 from model.agents.firm import Firm
 from model.agents.government import Government
@@ -15,37 +15,33 @@ def model():
 
 
 @pytest.fixture
-def govt(model):
+def firm(model):
     # Given
-    govt = Government(model)
-    govt.tax_rate = 0.25
-    model.governments = {"any": govt}
-    return govt
-
-
-@pytest.fixture
-def firm(model, monkeypatch):
-    # Given
-    monkeypatch.setattr(Firm, "dep_interests", PropertyMock(return_value=30))
     firm = Firm(model)
-    firm.country = "any"
-    firm.sales = 1000
-    firm.productivity = 2
-    firm.wage_offer = 20
-    firm.inventories = 60
-    firm.prev_inventories = 50
-    firm.loan_interest = 30
-    firm.wage_bill = 300
-    firm.rd = 100
+    firm.setup()
+    firm.roles["company"] = Mock()
+    firm.roles["producer"] = Mock()
+    firm.account = EcoAccount(model)
+    firm.account.setup()
     return firm
 
 
-def test_firm_compute_profit_distribution(firm, govt):
+def test_firm_compute_profit_distribution(firm):
+    # Given
+    firm.roles["company"].get_tax_rate.return_value = 0.25
+    firm.roles["producer"].productivity = 2
+    firm.roles["producer"].inventories = 60
+    firm.account.flows["dep_interests"] = 30
+    firm.account.flows["consumption"] = 1000
+    firm.account.flows["loan_interests"] = 30
+    firm.account.flows["wages"] = 400
+    firm.prev_inventories = 50
+    firm.wage_offer = 20
+
     # When
     firm.compute_profit_distribution()
 
     # Then
-    assert govt.taxes == 0
     assert firm.profit == pytest.approx(700)
     assert firm.net_cash_flow == pytest.approx(600)
     assert firm.taxes_payable == pytest.approx(150.0)
@@ -53,25 +49,30 @@ def test_firm_compute_profit_distribution(firm, govt):
 
 
 @pytest.fixture
-def bank(model, monkeypatch):
+def bank(model):
     # Given
-    monkeypatch.setattr(Bank, "bond_interests", PropertyMock(return_value=20))
-    monkeypatch.setattr(Bank, "dep_interests", PropertyMock(return_value=30))
     bank = Bank(model)
-    bank.country = "any"
-    bank.loan_interest = 100
-    bank.reserve_interest = 10
-    bank.bad_debt = 10
-    bank.cash_advance_interest = 10
+    bank.setup()
+    bank.roles["company"] = Mock()
+    bank.account = EcoAccount(model)
+    bank.account.setup()
     return bank
 
 
-def test_bank_compute_profit_distribution(bank, govt):
+def test_bank_compute_profit_distribution(bank):
+    # Given
+    bank.roles["company"].get_tax_rate.return_value = 0.25
+    bank.account.flows["bond_interests"] = 20
+    bank.account.flows["dep_interests"] = 30
+    bank.account.flows["loan_interests"] = 100
+    bank.account.flows["cash_interests"] = 10
+    bank.bad_debt = 10
+    bank.account.flows["adv_interests"] = 10
+
     # When
     bank.compute_profit_distribution()
 
     # Then
-    assert govt.taxes == 0
     assert bank.profit == pytest.approx(80)
     assert bank.taxes_payable == pytest.approx(20)
     assert bank.dividends_payable == pytest.approx(6)
